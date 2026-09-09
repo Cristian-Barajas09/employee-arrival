0f4a7abef0f61de800bd082e78140272c3db1486
Cristian-Barajas09 <cristianbarajasimasc@gmail.com>
2026-09-09 19:06:27 -0400

    feat(pipe): add ParseMongoIdPipe for validating MongoDB ObjectIds

---
+++ src/common/pipes/parse-mongo-id/parse-mongo-id.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { ParseMongoIdPipe } from './parse-mongo-id.pipe.js';
+
+describe('ParseMongoIdPipe', () => {
+  it('should be defined', () => {
+    expect(new ParseMongoIdPipe()).toBeDefined();
+  });
+});
