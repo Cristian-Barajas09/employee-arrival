ce478e0d687c4ce3cb493fefb24aac0a269222f6
Cristian-Barajas09 <cristianbarajasimasc@gmail.com>
2026-09-09 19:18:48 -0400

    feat(database): add MongoDB configuration and docker-compose setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,34 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module.js';
 import { QrModule } from './qr/qr.module.js';
 import { AuthModule } from './auth/auth.module.js';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { appConfig } from './config/application.config.js';
 import { validationSchema } from './config/zod.config.js';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [appConfig],
       validationSchema,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory(config: ConfigService) {
+        const user = config.getOrThrow<string>("database.user");
+        const password = config.getOrThrow<string>("database.password");
+        const host = config.getOrThrow<string>("database.host");
+        const port = config.getOrThrow<string>("database.port");
+        const database = config.getOrThrow<string>("database.dbName");
+
+
+
+
+        return {
+            uri: `mongodb://${user}:${password}@${host}:${port}/${database}`
+          }
+      },
     }),
     UsersModule,
     QrModule,
