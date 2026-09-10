c36dca76017ba22f67cc5f2b702d86ac66ab9d2f
Cristian-Barajas09 <cristianbarajasimasc@gmail.com>
2026-09-09 20:11:29 -0400

    feat(auth): implement authentication decorators and guards for role-based access control

---
+++ src/auth/decorators/auth.decorator.ts
@@ -0,0 +1,12 @@
+import { applyDecorators, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { UserRole } from '../../users/schemas/user.schema.js';
+import { RoleProtected } from './role-protected.decorator.js';
+import { UserRoleGuard } from '../guards/user-role.guard.js';
+
+export function Auth(...roles: UserRole[]) {
+  return applyDecorators(
+    RoleProtected(...roles),
+    UseGuards(AuthGuard(), UserRoleGuard),
+  );
+}
---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,36 @@
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { User } from "../../users/schemas/user.schema.js";
+import { JwtPayload } from "../interfaces/jwt-payload.interface.js";
+import { UsersService } from "../../users/users.service.js";
+import { ConfigService } from "@nestjs/config";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy( Strategy ) {
+
+    public constructor(
+        private readonly usersService: UsersService,
+        private readonly configService: ConfigService
+    ) {
+        super({
+            secretOrKey: configService.getOrThrow<string>('jwtSecret'),
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+        });
+    }
+
+    public async validate(payload: JwtPayload): Promise<User> {
+        const { id } = payload
+
+        const user = await this.usersService.findOneById(id)
+
+        if (!user || !id) {
+            throw new UnauthorizedException('Token not valid')
+        }
+
+
+        return user;
+    }
+
+
+}
