87c2a2357a16df687e05cccc7c3d54e2dbbb49d8
Cristian-Barajas09 <cristianbarajasimasc@gmail.com>
2026-09-10 13:34:02 -0400

    feat(auth): create AuthController for user registration, login, and auth status check

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,45 @@
+
+import { Body, Controller, Get, Headers, HttpCode, HttpStatus, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service.js";
+import { CreateUserDto } from "../users/dto/create-user.dto.js";
+import { LoginUserDTO } from "./dto/login-user.dto.js";
+
+import { GetUser } from "./decorators/get-user.decorator.js";
+import { type UserDocument, UserRole } from "../users/schemas/user.schema.js";
+
+import { Auth } from "./decorators/auth.decorator.js";
+
+
+@Controller('auth')
+export class AuthController {
+    public constructor(
+        private readonly authService: AuthService,
+    ) {}
+
+
+    @Post('register')
+    public register(
+        @Body()
+        createUserDTO: CreateUserDto
+    ) {
+
+        return this.authService.register(createUserDTO)
+    }
+
+    @Post('login')
+    @HttpCode(HttpStatus.OK)
+    public loginUser(
+        @Body() loginUserDTO: LoginUserDTO
+    ) {
+        return this.authService.login(loginUserDTO)
+    }
+
+
+    @Get('check-auth-status')
+    @Auth()
+    public checkAuthStatus(
+        @GetUser() user: UserDocument
+    ) {
+        return this.authService.checkAuthStatus(user);
+    }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,7 @@
-import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { Controller, Get, Param } from "@nestjs/common";
 import { UsersService } from "./users.service.js";
 import { ParseMongoIdPipe } from "../common/pipes/parse-mongo-id/parse-mongo-id.pipe.js";
-import { CreateUserDto } from "./dto/create-user.dto.js";
-import { Auth } from "../auth/decorators/auth.decorator.js";
-import { UserRole } from "./schemas/user.schema.js";
+
 
 @Controller("users")
 export class UsersController {
@@ -22,12 +20,6 @@ export class UsersController {
         @Param("id", ParseMongoIdPipe) id: string
     ) {
         return this.usersService.findOneById(id);
-    }
-
-    @Post()
-    @Auth(UserRole.ADMIN)
-    public async create(@Body() user: CreateUserDto) {
-        return this.usersService.create(user);
     }
 
 }
